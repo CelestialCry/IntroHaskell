d8b80170a9751b12df56393bb2aa27e49337dfcb
CelestialCry <thomas.w.thorbjornsen@gmail.com>
2019-11-14 00:20:55 +0100

    almost done

--- Types/Newtype.hs
+++ Types/Newtype.hs
@@ -1,8 +1,29 @@
 module Newtype where
 
+import Typeclasses
+
 -- A way to "wrap" our algebraic data types (create new types)
 -- These are very restricted, but fats
 -- There are a lot of use cases for newtypes, but that is a story of it's own
 
-newtype Add = Add Int
+
+newtype Mult = Mult {unMult :: Int}
 -- This allows us to redefine our typeclasses for the Int type
+
+instance Semigroup Mult where
+    a <> b = Mult $ unMult a * unMult b
+
+
+
+newtype Numeral = Numeral {getInt :: Int}
+
+instance JSNess Numeral where
+    toBool Numeral {getInt = 1} = True 
+    toBool _ = False
+
+
+
+newtype State s a = State {getState :: s -> (a,s)}
+-- We use newtypes to define states in Haskell, like this
+-- But we have to add a bit more structure so that it
+-- actually works
